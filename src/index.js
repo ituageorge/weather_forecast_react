7cb58c8cd1ded24e68b368f912210fb2002b8e00
ituageorge <50578363+ituageorge@users.noreply.github.com>
2020-10-17 19:55:36 +0100

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-import WeatherForecast from './js/components/weather_forecast';
-
-const wrapper = document.getElementById('container');
-wrapper ? ReactDOM.render(<WeatherForecast />, wrapper) : false;
